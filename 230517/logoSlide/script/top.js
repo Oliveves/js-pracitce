fda938ac14a1172a2ad9a452939ba11ef6736ce9
oliveves <oliveves@gmail.com>
2023-05-17 13:01:05 +0900

    내 로고로 슬라이드 만들기 (jquery)

---
+++ 230517/logoSlide/script/top.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var x = 0;
+    setInterval (function (){
+        if(x<2){
+            x++;
+        } else {
+            x=0;
+        }
+
+        var sp = x * (-300)+"px";
+        $(".slideList").animate({top:sp}, 400);
+    console.log(x);
+    }, 3000);
+});
